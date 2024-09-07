3c7153554cab0eceb1e109679d28aed7393bd93a
devpull <devpull@yandex.ru>
2024-09-07 16:52:11 +0300

    Changed base for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/velocity.github.io/',
   esbuild: {
     drop: ['console', 'debugger']
   },
